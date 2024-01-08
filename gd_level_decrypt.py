69e73bbad068321a5b2e1516679bf1b641ce8bc0
Erik Swanson <eswanson224@gmail.com>
2024-01-08 12:03:54 -0700

    cleaned up the file

--- gd_level_decrypt.py
+++ gd_level_decrypt.py
@@ -3,26 +3,20 @@ from lxml import etree
 import os
 import zlib
 
-
 class GetLevel:
     def __init__(self, lvl):
-        self.name: str = getk(lvl, 2, '')
-        self.desc: str = base64.b64decode(getk(lvl, 3, '')).decode()
-        self.data: str = decrypt(getk(lvl, 4, ''))
-        self.attempts: int = int(getk(lvl, 18, 0))
-        self.length: int = int(getk(lvl, 23, 0))
-        self.jumps: int = int(getk(lvl, 36, 0))
-        self.copy: int = int(getk(lvl, 42, 0))
-        self.song: int = int(getk(lvl, 45, 0))
-        self.objCount: int = int(getk(lvl, 48, 0))
+        self.name: str = getk(lvl, 2) or ''
+        self.desc: str = base64.b64decode(getk(lvl, 3)).decode() or ''
+        self.data: str = decrypt(getk(lvl, 4)) or ''
+        self.attempts: int = int(getk(lvl, 18)) or 0
+        self.length: int = int(getk(lvl, 23)) or 0
+        self.jumps: int = int(getk(lvl, 36)) or 0
+        self.copy: int = int(getk(lvl, 42)) or 0
+        self.song: int = int(getk(lvl, 45)) or 0
+        self.objCount: int = int(getk(lvl, 48)) or 0
 
-
-def getk(element, num, default):
-    if element.find(f"k[.='k{num}']") is None:
-        return default
-    else:
-        return element.find(f"k[.='k{num}']").getnext().text
-
+def getk(element, num):
+    return element.find(f"k[.='k{num}']").getnext().text
     '''
     2 = title
     3 = description(base64)
@@ -36,7 +30,6 @@ def getk(element, num, default):
     48 = object count
     '''
 
-
 def decrypt(text) -> str:
     text = text.replace('-', '+').replace('_', '/')
     textbytes = zlib.decompress(base64.b64decode(text)[10:], -zlib.MAX_WBITS)
@@ -44,35 +37,26 @@ def decrypt(text) -> str:
 
 
 def xor(path, key) -> str:
-    fr = open(path, 'rb')
-    data = fr.read()
-    fr.close()
+    with open(path, 'rb') as fr:
+        data = fr.read()
 
     res = []
     for i in data:
         res.append(i ^ key)
     return bytearray(res).decode()
 
-
 if __name__ == '__main__':
+    # Windows save path
     LocalLevels = f"{os.getenv('LOCALAPPDATA')}\\GeometryDash\\CCLocalLevels.dat"
     GameManager = f"{os.getenv('LOCALAPPDATA')}\\GeometryDash\\CCGameManager.dat"
     save = decrypt(xor(LocalLevels, 11))
     root = etree.fromstring(save)
     
-    levels = root[0][1].findall('d')
-    
-    print("1,8,2,285,3,15") # something, blockID, something, xPos, something, something
-    print(GetLevel(levels[7]).data.split(';')[1])
+    levelsXml = root[0][1].findall('d')
 
-'''
-fw = open('files/save.txt', 'wb')
-fw.write(etree.tostring(root, pretty_print=True))
-fw.close()
+    levels = []
+    for level in levelsXml:
+        levels.append(getLevel(level))
 
-
-
-for lvl in levels:
-    l = GetLevel(lvl)
-    print(l.name)
-'''
+    for level in levels:
+        print(level.name)
